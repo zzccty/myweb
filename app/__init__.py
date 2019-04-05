19a8aad93aacb34f2dd695c5ae3220f1bfd7508e
lovekernel <busui@tp.edu.tw>
2019-04-05 23:50:35 +0800

    finished test text-hide

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 from config import Config
+from flask_bootstrap import Bootstrap
 
 
 app = Flask('app')
 app.config.from_object(Config)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def sayHello():
-    return r'Hello world!'
+    return render_template('index.html')
 
 print(app.config['SECRET_KEY'])
 
