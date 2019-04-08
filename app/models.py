d5b131b96fca596b43a11984b8237e24e7ba795d
lovekernel <busui@tp.edu.tw>
2019-04-08 14:07:59 +0800

    initialized models.py

--- app/models.py
+++ app/models.py
@@ -23,6 +23,10 @@ class User(db.Model):
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+tags = db.Table('tags',
+    db.Column('tag_id', db.Integer, db.ForeignKey('tags.id'), primary_key=True),
+    db.Column('post_id', db.Integer, db.ForeignKey('posts.id'), primary_key=True)
+)
 
 class Post(db.Model):
     __tablename__ = 'posts'
@@ -31,15 +35,24 @@ class Post(db.Model):
     body = db.Column(db.Text)
     timestamp = db.Column(db.DaTetime, index=True, default=datetime.utcnow)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
+    reading_volume = db.Column(db.Integer, default=0)
+    categories = db.relationship('Category', backref='posts', lazy=True)
+    tags = db.relationship('Tag', secondary=tags, lazy='subquery',
+                                 backref=db.backref('posts', lazy=True))
 
 
 class Tag(db.Model):
     __tablename__ = 'tags'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64))
+    tab_name = db.Column(db.String(64))
 
 
 
+class Category(db.Model):
+    __tablename__ = 'categories'
+    id = db.Column(db.Integer, primary_key=True)
+    category_name = db.Column(db.String(64), nollable=False)
 
 
 
