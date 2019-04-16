2a12416f40d176a2117a0b809579bdeec4d61df3
lovekernel <busui@tp.edu.tw>
2019-04-16 22:25:25 +0800

    add log

--- app/__init__.py
+++ app/__init__.py
@@ -5,11 +5,14 @@ from flask_login import LoginManager
 from flask_pagedown import PageDown
 from flask_moment import Moment
 import os
+import logging
+from logging.handlers import RotatingFileHandler
 
 # import local object/module
 from config import config
 
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 db = SQLAlchemy()
 bootstrap = Bootstrap()
@@ -17,6 +20,7 @@ login_manager = LoginManager()
 login_manager.login_view = 'main.login'
 moment = Moment()
 pagedown = PageDown()
+
 
 
 def create_app(config_name):
@@ -33,9 +37,33 @@ def create_app(config_name):
     moment.init_app(app)
     pagedown.init_app(app)
 
+    register_logging(app)
+
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
 
     return app
 
+def register_logging(app):
+    class RequestFormatter(logging.Formatter):
+
+        def format(self, record):
+            record.url = request.url
+            record.remote_addr = request.remote_addr
+            return super(RequestFormatter, self).format(record)
+
+    request_formatter = RequestFormatter(
+        '[%(asctime)s] %(remote_addr)s requested %(url)s\n'
+        '%(levelname)s in %(module)s: %(message)s'
+    )
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    file_handler = RotatingFileHandler(os.path.join(basedir, 'logs/love.log'),
+                                       maxBytes=10 * 1024 * 1024, backupCount=10)
+    file_handler.setFormatter(formatter)
+    file_handler.setLevel(logging.INFO)
+
+    if not app.debug:
+        app.logger.addHandler(file_handler)
