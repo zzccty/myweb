198edb54247e49b363ddf451d378a136312fcb4f
lovekernel <busui@tp.edu.tw>
2019-04-17 14:48:43 +0800

    优化

--- myweb.py
+++ myweb.py
@@ -2,9 +2,13 @@ import os
 from app import create_app, db
 from flask_migrate import Migrate
 from app.models import User, Tag, Category, Post, Category_by_date
+from dotenv import load_dotenv
 
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
 
-app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+app = create_app(os.getenv('FLASK_CONFIG') or 'production')
 migrate = Migrate(app, db)
 
 
