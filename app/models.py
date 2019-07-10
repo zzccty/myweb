3328f7fd0f718a12cec73ad003b2634a4a39017a
Busui <wuqingcong@gmail.com>
2019-07-11 00:08:44 +0800

    update something to new

--- app/models.py
+++ app/models.py
@@ -129,9 +129,3 @@ class Category_by_date(db.Model):
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
-
-
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
